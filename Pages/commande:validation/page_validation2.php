cc0cb052ee237cc4bb6d3e3e4e1dd6dd99508cc7
Antonain Ebt <antoebtf@hotmail.com>
2019-09-20 10:18:14 +0200

    1er commit new Branche Com/Val -- 2 pages liées et OP

---
+++ Pages/commande:validation/page_validation2.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Wasabi Coffee </title>
+    <link href="navfooter.css" type="text/css" rel="stylesheet">
+    <link href="style2.css" type="text/css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Permanent+Marker&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Reem+Kufi&display=swap" rel="stylesheet">
+    <link href="burger.css" type="text/css" rel="stylesheet">
+
+</head>
+
+<body>
+    <header id="test">
+        <ul id="foo">
+            <li id="titre">
+                <h1><a href="index.html">WASABI</a></h1>
+            </li>
+            <li class="linker"><a href="#">A la carte </a></li>
+            <li class="linker"><a href="#">Le restaurant</a> </li>
+            <li class="linker"><a href="#">Contact</a> </li>
+        </ul>
+        <label class="switch">
+            <input type="checkbox" onclick="return toggle();">
+            <span class="slider round"></span>
+        </label>
+    </header>
+
+    <!-- banniere à remplir -->
+    <section id="banniere">
+        <h1> WASABI </h1>
+    </section>
+
+    <!-- content -->
+    <section id="content">
+        <div>
+            <img id="ico-val" src="https://pngimage.net/wp-content/uploads/2018/06/validation-png.png">
+            <h2> VOTRE COMMANDE A BIEN ETE EFFECTUEE</h2>
+        </div>
+        <footer>
+            <ul>
+                <li>
+                    <ul>
+                        <li><a href="#">Contact@wasabicafe.fr</a></li>
+                        <li><a href="#">36 30 36 30</a></li>
+                        <li><a href="#">Voir plan d'accès</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <ul>
+                        <li><a href="#">Mentions légales</a></li>
+                        <li><a href="#">A la carte</a></li>
+                        <li><a href="#">Le restaurant</a></li>
+                    </ul>
+                </li>
+            </ul>
+
+        </footer>
+    </section>
+
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="script.js"></script>
+    <script src="burger.js"></script>
+</body>
+
+</html>
